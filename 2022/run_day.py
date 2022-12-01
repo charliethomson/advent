88aba2eade50c1ca03bdd7c3b245d0b4fdc6c48b
Charlie Thomson <charlie@thmsn.dev>
2022-11-30 23:12:46 -0500

    make mix project, fix no stdout in run_day

--- 2022/run_day.py
+++ 2022/run_day.py
@@ -3,25 +3,34 @@ import csv
 import subprocess
 from termcolor import colored
 
+
 def get_result(result: bytes) -> str:
     result = result.decode("utf8")
+    if len(result.strip()) == 0:
+        return None
     if "RESULT: " in result:
         return str(result).split("RESULT: ").pop(0).strip()
     else:
         return str(result).strip().splitlines().pop().strip()
 
-def run(day: int | None, part: int | None, expected: int | None, disable_suppression: bool = False):
+
+def run(
+    day: int | None,
+    part: int | None,
+    expected: int | None,
+    disable_suppression: bool = False,
+):
     if day is None:
-        for d in range(1,26):
+        for d in range(1, 26):
             run(d, part, expected)
         return
-    
+
     if part is None:
         for p in range(1, 3):
             run(day, p, expected)
         return
-    
-    cmd = ['elixir', f'./day_{day}/solution.part{part}.exs']
+
+    cmd = ["mix", "run", f"./day_{day}/solution.part{part}.exs"]
     print(f"Running day {day} : part {part}: ", end="")
 
     stderr = None
@@ -32,22 +41,36 @@ def run(day: int | None, part: int | None, expected: int | None, disable_suppres
 
     if expected is None:
         print(colored("Ok.", "green"))
-        print(f"Got:\n\t\"{result}\"")
+        print(f'Got:\n\t"{result}"')
         return
 
     if result == expected:
         print(colored("Ok.", "green"))
     else:
         print(colored("Err", "red"))
-        print(f"Expected:\n\t\"{expected}\"")
-        print(f"Got:\n\t\"{result}\"")
+        print(f'Expected:\n\t"{expected}"')
+        print(f'Got:\n\t"{result}"')
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='AOC Runner',description='Run AOC solutions (elixir)')
-    parser.add_argument('-d', '--day', action="store", help="The day to run, omit for all", type=int)
-    parser.add_argument('-p', '--part', action="store", help="The part to run, omit for all", type=str)
-    parser.add_argument('-x', '--expected', action="store", help="Expected result, omit for no checking", type=str)
-    parser.add_argument('--no-suppress', action="store_true", help="Disable stderr suppression")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="AOC Runner", description="Run AOC solutions (elixir)"
+    )
+    parser.add_argument(
+        "-d", "--day", action="store", help="The day to run, omit for all", type=int
+    )
+    parser.add_argument(
+        "-p", "--part", action="store", help="The part to run, omit for all", type=str
+    )
+    parser.add_argument(
+        "-x",
+        "--expected",
+        action="store",
+        help="Expected result, omit for no checking",
+        type=str,
+    )
+    parser.add_argument(
+        "--no-suppress", action="store_true", help="Disable stderr suppression"
+    )
     args = parser.parse_args()
     run(args.day, args.part, args.expected, args.no_suppress)
