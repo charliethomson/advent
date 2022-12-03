4c7aa15427df4223ab894654901b7b639939e762
Charlie Thomson <charlie@thmsn.dev>
2022-12-03 10:39:14 -0500

    day 3

--- 2022/day_3/solution.part1.exs
+++ 2022/day_3/solution.part1.exs
@@ -1,11 +1,43 @@
+defmodule Ruck do
+  def split(line) do
+    String.split_at(line, round(String.length(line) / 2))
+  end
+
+  def find({cmp1, cmp2}) do
+    set = MapSet.new(String.codepoints(cmp1))
+
+    String.codepoints(cmp2)
+    |> Enum.find(fn item -> MapSet.member?(set, item) end)
+  end
+
+  def score(letter) do
+    ch = String.to_charlist(letter) |> List.first()
+
+    if(ch > 91,
+      do: ch - 96,
+      else: ch - 38
+    )
+  end
+
+  def run(lines) do
+    lines
+    |> Enum.map(&Ruck.split/1)
+    |> Enum.map(&Ruck.find/1)
+    |> Enum.map(&Ruck.score/1)
+    |> Enum.sum()
+  end
+end
 
 simple_lines =
-    "./day_3/input.simple.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
+  "./day_3/input.simple.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Ruck.run()
+  |> IO.puts()
 
 problem_lines =
-    "./day_3/input.problem.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
-    
+  "./day_3/input.problem.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Ruck.run()
+  |> IO.puts()
--- 2022/day_3/solution.part2.exs
+++ 2022/day_3/solution.part2.exs
@@ -1,11 +1,44 @@
+defmodule Ruck do
+  def find(group) do
+    sets = group |> Enum.map(&MapSet.new(String.codepoints(&1)))
+
+    set =
+      sets
+      |> Enum.reduce(nil, fn cur, acc ->
+        if(acc == nil, do: cur, else: MapSet.intersection(acc, cur))
+      end)
+
+    set |> MapSet.to_list() |> List.first()
+  end
+
+  def score(letter) do
+    ch = String.to_charlist(letter) |> List.first()
+
+    if(ch > 91,
+      do: ch - 96,
+      else: ch - 38
+    )
+  end
+
+  def run(lines) do
+    lines
+    |> Enum.chunk_every(3)
+    |> Enum.map(&Ruck.find/1)
+    |> Enum.map(&Ruck.score/1)
+    |> Enum.sum()
+  end
+end
 
 simple_lines =
-    "./day_3/input.simple.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
+  "./day_3/input.simple.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Ruck.run()
+  |> IO.puts()
 
 problem_lines =
-    "./day_3/input.problem.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
-    
+  "./day_3/input.problem.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Ruck.run()
+  |> IO.puts()
