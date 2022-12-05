afad82bb0b8e162d2b9bf84699366df43348e3bf
Charlie Thomson <charlie@thmsn.dev>
2022-12-05 11:36:43 -0500

    day 5

--- 2022/day_5/solution.part2.exs
+++ 2022/day_5/solution.part2.exs
@@ -1,11 +1,88 @@
+#     [D]
+# [N] [C]
+# [Z] [M] [P]
+#  1   2   3
+initial_state_simple = [
+  [:z, :n],
+  [:m, :c, :d],
+  [:p]
+]
 
-simple_lines =
-    "./day_5/input.simple.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
+#             [C]         [N] [R]
+# [J] [T]     [H]         [P] [L]
+# [F] [S] [T] [B]         [M] [D]
+# [C] [L] [J] [Z] [S]     [L] [B]
+# [N] [Q] [G] [J] [J]     [F] [F] [R]
+# [D] [V] [B] [L] [B] [Q] [D] [M] [T]
+# [B] [Z] [Z] [T] [V] [S] [V] [S] [D]
+# [W] [P] [P] [D] [G] [P] [B] [P] [V]
+#  1   2   3   4   5   6   7   8   9
+initial_state_problem = [
+  [:w, :b, :d, :n, :c, :f, :j],
+  [:p, :z, :v, :q, :l, :s, :t],
+  [:p, :z, :b, :g, :j, :t],
+  [:d, :t, :l, :j, :z, :b, :h, :c],
+  [:g, :v, :b, :j, :s],
+  [:p, :s, :q],
+  [:b, :v, :d, :f, :l, :m, :p, :n],
+  [:p, :s, :m, :f, :b, :d, :l, :r],
+  [:v, :d, :t, :r]
+]
 
-problem_lines =
-    "./day_5/input.problem.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
-    
+defmodule HanoiLike do
+  def mv([count, from, to], state) do
+    src = Enum.fetch!(state, from)
+    split_index = length(src) - count
+
+    {car, cdr} = Enum.split(src, split_index)
+
+    res =
+      Enum.with_index(state)
+      |> Enum.map(fn {column, index} ->
+        case index do
+          _ when index == from -> car
+          _ when index == to -> column ++ cdr
+          _ -> column
+        end
+      end)
+
+    res
+  end
+
+  def parse_instructions(instruction) do
+    re = ~r/move (?<count>\d+) from (?<from>\d+) to (?<to>\d+)/
+    caps = Regex.named_captures(re, instruction)
+
+    caps
+    |> Map.to_list()
+    |> Enum.map(fn {_, n} -> String.to_integer(n) end)
+    |> Enum.with_index()
+    |> Enum.map(fn {el, index} -> if(index == 0, do: el, else: el - 1) end)
+  end
+end
+
+simple =
+  "./day_5/input.simple.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&HanoiLike.parse_instructions/1)
+  |> Enum.reduce(initial_state_simple, &HanoiLike.mv/2)
+  |> Enum.reduce("", fn cur, acc ->
+    top = List.last(cur) |> to_string() |> String.upcase()
+    "#{acc}#{top}"
+  end)
+
+IO.puts(simple)
+
+problem =
+  "./day_5/input.problem.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&HanoiLike.parse_instructions/1)
+  |> Enum.reduce(initial_state_problem, &HanoiLike.mv/2)
+  |> Enum.reduce("", fn cur, acc ->
+    top = List.last(cur) |> to_string() |> String.upcase()
+    "#{acc}#{top}"
+  end)
+
+IO.puts(problem)
