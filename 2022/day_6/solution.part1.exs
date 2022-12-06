c669b8e9043f809b86c39955e5b3f4ed73377fa3
Charlie Thomson <charlie@thmsn.dev>
2022-12-06 02:12:17 -0500

    day 6

--- 2022/day_6/solution.part1.exs
+++ 2022/day_6/solution.part1.exs
@@ -1,11 +1,38 @@
+defmodule Signal do
+  def find_first_marker(packet) do
+    packet
+    |> String.to_charlist()
+    |> Enum.with_index()
+    |> Enum.reduce_while([], fn
+      {_, index}, acc when length(acc) == 4 ->
+        {:halt, index}
+
+      {ch, index}, acc ->
+        split_index = acc |> Enum.find_index(fn el -> el == ch end)
+
+        case split_index do
+          nil ->
+            {:cont, acc ++ [ch]}
+
+          _ ->
+            {s, e} = Enum.split(acc, split_index + 1)
+            {:cont, e ++ [ch]}
+        end
+    end)
+  end
+end
 
 simple_lines =
-    "./day_6/input.simple.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
+  "./day_6/input.simple.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&Signal.find_first_marker/1)
+  |> Enum.each(&IO.puts/1)
 
 problem_lines =
-    "./day_6/input.problem.txt"
-    |> File.stream!()
-    |> Enum.map(&String.trim/1)
-    
+  "./day_6/input.problem.txt"
+  |> File.stream!()
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&Signal.find_first_marker/1)
+  |> Enum.each(&IO.puts/1)
